b50b8d9a7c4f8b9f7d71c73fd08a043358c68529
DevOpsMina <devmina635@gmail.com>
2024-07-30 11:43:36 +0545

    minadd

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ function App() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('api-mina.learn.cloudlaya.com/api/data');
+      const response = await axios.get('api-mina.learn.cloudlaya.com/api/data:5001');
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -177,7 +177,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('api-mina.learn.cloudlaya.com/api/data', { name, description });
+      await axios.post('api-mina.learn.cloudlaya.com/api/data:5001', { name, description });
       alert('Data added successfully!');
       setName('');
       setDescription('');
